93930f77219eb18c001853d65186b3b14bd27bf2
Z-Jarrah <jarrahzeed@gmail.com>
2015-03-08 23:50:05 -0700

    Progress on serial download

--- CPSC351/serialfd.cpp
+++ CPSC351/serialfd.cpp
@@ -1,3 +1,14 @@
+//The serial downloader shall download files one by one. After the parent process has read and
+//parsed the urls.txt file, it shall proceed as follows:
+//1. The parent process forks off a child process.
+//2. The child uses execlp("/usr/bin/wget", "wget", <URL STRING1>, NULL) system call
+//in order to replace its program with wget program that will download the first file in
+//urls.txt (i.e. the file at URL <URL STRING1>).
+//3. The parent executes a wait() system call until the child exits.
+//4. The parent forks off another child process which downloads the next file specified in
+//urls.txt.
+//5. Repeat the same process until all files are downloaded.
+
 #include <iostream>
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,6 +21,13 @@ using namespace std;
 
 int main()
 {
-    
-    retun 0;
+    pid_t pid;
+    pid = fork();
+    if(pid == 0)
+    {
+        execlp("/usr/bin/wget", "wget","http://www.cisco.com/networkers/nw04/presos/docs/NMS-1N02.pdf", NULL);
+    }
+    else
+        wait(NULL);
+    return 0;
 }
