e6177c3318e91f266ded6c88324050e1702dc925
Z-Jarrah <jarrahzeed@gmail.com>
2015-03-09 01:36:17 -0700

    Working on design of parallelfd

--- CPSC351/ParallelFileDownloader/parallelfd.cpp
+++ CPSC351/ParallelFileDownloader/parallelfd.cpp
@@ -1,3 +1,12 @@
+//Parallel Downloader
+//1. The parent forks off n children, where n is the number of URLs in urls.txt.
+//2. Each child executes execlp("/usr/bin/wget", "wget", <URL STRING>, NULL) system
+//call where each <URL STRING> is a distinct URL in urls.txt.
+//3. The parent calls wait() (n times in a row) and waits for all children to terminate.
+//4. The parent exits.
+//while the parallel downloader executes, the outputs from different children may
+//intermingle. This is acceptable.
+
 #include <iostream>
 #include <unistd.h>
 #include <sys/types.h>
@@ -12,10 +21,22 @@ using namespace std;
 
 int main()
 {
-    char url[250]
+    char url[250];
     ifstream inFile;
     pid_t pid;
     
+    inFile.open("URLS for Assignment - 1.txt");
+    if(!inFile.is_open())
+    {
+        cout << "Failed to open file!\n";
+    }
+    fork();
+    fork();
+    fork();
+    fork();
+    fork();
+    fork();
+    fork();
     
     return 0;
 }
