704045306b70f9239813373b86e89cefef18050e
Zeed Jarrah <jarrahzeed@gmail.com>
2015-03-15 18:53:24 -0700

    Problem 4
    First rough solution

--- CPSC335/Assignment 4/prob4.cpp
+++ CPSC335/Assignment 4/prob4.cpp
@@ -11,7 +11,7 @@ struct Node
 	Node* next;
 };
 
-Node* InsertWord(Node* h[], string word);
+void InsertWord(Node* h[], string word[]);
 void SortHash(Node*& h);
 
 int main()
@@ -23,20 +23,16 @@ int main()
 	{
 		hashTable[i] = NULL;
 	}
-	
-	for(int i = 0; i < 16; i++)
-	{
-		int k = int(words[i].at(0)) - 65;
-		hashTable[k] = InsertWord(hashTable, string(words[i]));
-	}
+		
+	InsertWord(hashTable, words);
 
-	for(int i = 0; i < 26; i++)
-	{
-		if(hashTable[i] != NULL)
-		{
-			SortHash(hashTable[i]);
-		}
-	}
+	//for(int i = 0; i < 26; i++)
+	//{
+	//	if(hashTable[i] != NULL)
+	//	{
+	//		SortHash(hashTable[i]);
+	//	}
+	//}
 
 	//For debugging purposes, needs to be its own function
 	int numOfwords = 0;
@@ -55,47 +51,81 @@ int main()
 	return 0;
 }
 
-Node* InsertWord(Node* h[], string word)
+void InsertWord(Node* h[], string word[])
 {
-	int index = int(word.at(0)) - 65;
-	cout << "index: " << index << endl;
-	Node* t = new Node;
-	t->info = word;
-	t->next = NULL;
+	string tmparr[16];
 
-	if(h[index] == 0)
+	for(int k = 2; k >= 0; k--)
 	{
-		h[index] = t;
-	}
-	else
-	{
-		Node* tmp = h[index];
-		while(tmp->next != NULL)
+		for(int i = 0; i < 16; i++)
 		{
-			tmp = tmp->next;
+			int index = int(word[i].at(k)) - 65;
+			cout << "letter: " << word[i].at(k) << " index: " << index << endl;
+			Node* t = new Node;
+			t->info = word[i];
+			t->next = NULL;
+
+			if(h[index] == 0)
+			{
+				h[index] = t;
+			}
+			else
+			{
+				Node* tmp = h[index];
+				while(tmp->next != NULL)
+				{
+					tmp = tmp->next;
+				}
+				tmp->next = t;
+			}
 		}
-		tmp->next = t;
+//
+//
+		if(k != 0)
+		{
+			int l = 0;
+			for(int j = 0; j < 26; j++)
+			{
+				Node* temp = h[j];
+				while (temp != NULL)
+				{
+					word[l] = temp->info;
+					temp = temp->next;
+					l++;
+				}
+
+				delete h[j];
+				h[j] = 0;
+			}
+		}
 	}
-	
-	return h[index];
 }
 
 void SortHash(Node*& h)
 {
 	//// Still needs a proper sort algorithm
-	Node* curr = h;
-	Node* prev;
+	//Node* curr = h;
+	//Node* prev = curr;
+	//int sizeOfList = 0;
+
+	//while(curr != NULL)
+	//{
+	//	curr = curr->next;
+	//	sizeOfList++;
+	//}
+
+	//cout << "Size of list in index: " << sizeOfList << endl;
 	//Sorting
-	if(curr != NULL && curr->next != NULL /*&& curr->next->info.compare(curr->info) < 0*/)
-	{
-		prev = curr;
-		curr = curr->next;
-		curr->next = prev;
-		h = curr;
-		prev->next = NULL;
-		cout << "curr " << curr->info << "prev " << prev->info << endl;
-		cout << "head is now " <<h->info << endl;
-	}
+	//if(curr != NULL && curr->next != NULL /*&& curr->next->info.compare(curr->info) < 0*/)
+	//{
+	//	prev = curr;
+	//	curr = curr->next;
+	//	curr->next = prev;
+	//	h = curr;
+	//	prev->next = NULL;
+	//	cout << "curr " << curr->info << "prev " << prev->info << endl;
+	//	cout << "head is now " <<h->info << endl;
+	//}
 	
 }
 
